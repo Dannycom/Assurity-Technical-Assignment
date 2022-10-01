926eee6e5ee076504a9f3020aaea781ea32ae185
Dannycom <58293876+Dannycom@users.noreply.github.com>
2022-10-02 02:40:23 +1300

    Criteria met
    Acceptance criteria met

---
+++ test_main.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+resp = requests.get("https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false") #getting request from API
+json_resp = resp.json() #formatting into json format
+
+#test for Name
+def test_name():
+    assert json_resp['Name'] == 'Badges'
+
+#test for CanRelist
+def test_canrelist():
+    assert json_resp['CanRelist'] is True
+
+#test for Promotions element
+def test_promo():
+    for ele in json_resp["Promotions"]:
+        if ele["Name"] == "Feature":
+            assert ele["Description"] == "Better position in category"
